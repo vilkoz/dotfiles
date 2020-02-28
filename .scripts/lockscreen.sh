21ad86d5bcdd7f9748772e5d89b1c1425d263dca
Vitalii Rybalko <vilko.a@gmail.com>
2020-02-28 21:34:18 +0200

    i3: use i3lock for lockscreen, and separate script with keyboard layout changing
    Signed-off-by: Vitalii Rybalko <vilko.a@gmail.com>

--- .scripts/lockscreen.sh
+++ .scripts/lockscreen.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 #cd ~/.scripts/scop; ./scop res/statue/statue.obj &
+i3-msg workspace 1;
+i3-msg workspace 8;
+i3-msg workspace 2;
+i3-msg workspace 9;
 setxkbmap -layout us
-slock
+i3lock -c 000000
 setxkbmap -layout us,ru -option grp:win_space_toggle
